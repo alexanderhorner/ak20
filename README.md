19291421c2b92db1df52276bdf949514ed09b2d2
Alexander Horner <mail@alexanderhorner.com>
2020-07-22 14:31:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## About AK20
+
+AK is an abbreviation for the German word "Abschlussklasse" and means senior year. The 20 is for the year 2020, in which I graduated.
+
+## Why?
+
+I made this website so all my classmates can stay connected, as well as to remember special moments that happened during our school time.
+
+## License
+
+Everything in this repository is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ resources/views/home.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+
+                <button>Test</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+{{-- 
+    
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/all.css') }}">
+        <style>
+            body {
+                position: fixed;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-family: 'Nunito', sans-serif;
+                color: hsl(0, 0%, 15%);
+            }
+        </style>
+    </head>
+    <body>
+        <div>AK20</div>
+    </body>
+</html>
+
+ --}}
