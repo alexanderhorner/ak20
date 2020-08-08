6b893a5c6cd8ae9a3cc7e7238598cb3834f64d5c
Alexander Horner <mail@alexanderhorner.com>
2020-08-08 20:46:37 +0200

    Built App

---
+++ public/2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{54:function(n,t,e){"use strict";e.r(t);var i={},s=e(1),l=Object(s.a)(i,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("Login")])}),[],!1,null,null,null);t.default=l.exports}}]);
